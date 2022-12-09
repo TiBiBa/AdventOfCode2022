d5e723290d2aba31fe018eeebc37b0da94fad281
Timon Bakker <timonbakker@hotmail.com>
2022-12-09 10:51:41 +0100

    Fixed part 1

---
+++ day 9/main.py
@@ -0,0 +1,61 @@
+def move_head(pos, direction):
+    if direction == "R":
+        pos = [pos[0] + 1, pos[1]]
+    elif direction == "D":
+        pos = [pos[0], pos[1] - 1]
+    elif direction == "L":
+        pos = [pos[0] - 1, pos[1]]
+    else:
+        pos = [pos[0], pos[1] + 1]
+    return pos
+
+
+def move_tail(pos_t, pos_h):
+    if pos_t[0] == pos_h[0]:
+        if pos_t[1] == pos_h[1] + 2:
+            pos_t[1] -= 1
+        elif pos_t[1] == pos_h[1] - 2:
+            pos_t[1] += 1
+    elif pos_t[1] == pos_h[1]:
+        if pos_t[0] == pos_h[0] + 2:
+            pos_t[0] -= 1
+        elif pos_t[0] == pos_h[0] - 2:
+            pos_t[0] += 1
+    else:
+        if pos_t[0] == pos_h[0] + 1 and pos_t[1] == pos_h[1] + 1:
+            return pos_t
+        elif pos_t[0] == pos_h[0] + 1 and pos_t[1] == pos_h[1] - 1:
+            return pos_t
+        elif pos_t[0] == pos_h[0] - 1 and pos_t[1] == pos_h[1] + 1:
+            return pos_t
+        elif pos_t[0] == pos_h[0] - 1 and pos_t[1] == pos_h[1] - 1:
+            return pos_t
+        else:
+            if pos_h[0] > pos_t[0] and pos_h[1] > pos_t[1]:
+                pos_t = [pos_t[0] + 1, pos_t[1] + 1]
+            elif pos_h[0] > pos_t[0] and pos_h[1] < pos_t[1]:
+                pos_t = [pos_t[0] + 1, pos_t[1] - 1]
+            elif pos_h[0] < pos_t[0] and pos_h[1] < pos_t[1]:
+                pos_t = [pos_t[0] - 1, pos_t[1] - 1]
+            elif pos_h[0] < pos_t[0] and pos_h[1] > pos_t[1]:
+                pos_t = [pos_t[0] - 1, pos_t[1] + 1]
+    return pos_t
+
+
+def part1():
+    with open("input.txt", mode="r", encoding="utf8") as file:
+        pos_h = [0, 0]
+        pos_t = [0, 0]
+        visited = set()
+
+        instructions = file.read().splitlines()
+        for instruction in instructions:
+            direction, length = instruction.split()
+            for i in range(0, int(length)):
+                pos_h = move_head(pos_h, direction)
+                pos_t = move_tail(pos_t, pos_h)
+                visited.add(tuple(pos_t))
+        print(len(visited))
+
+
+part1()
