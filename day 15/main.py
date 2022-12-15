e4f522ff1ea69411169cba8b7b0aca9a2f9db520
Timon Bakker <timonbakker@hotmail.com>
2022-12-15 12:55:13 +0100

    Part2 still in progress

--- day 15/main.py
+++ day 15/main.py
@@ -1,6 +1,5 @@
 sensors = set()
 beacons = set()
-non_beacon = set()
 
 
 def split_line(line):
@@ -22,16 +21,7 @@ def manhattan_distance(sensor, beacon):
     return distance
 
 
-def travel_sensor(coordinate, distance):
-    points = [(coordinate[0], coordinate[1] + 1), (coordinate[0] + 1, coordinate[1]),
-              (coordinate[0], coordinate[1] - 1), (coordinate[0] - 1, coordinate[1])]
-
-    for point in points:
-        non_beacon.add(point)
-        travel_sensor(point, distance - 1)
-
-
-def main():
+def main(part1):
     global sensors
     global beacons
     travels = {}
@@ -44,8 +34,44 @@ def main():
             beacons.add(beacon)
             travels[sensor] = manhattan_distance(sensor, beacon)
 
-        for coordinate, distance in travels.items():
-            travel_sensor(coordinate, distance)
+        # Part 1
+        y_points = set()
+        y_target = 2000000
+
+        if part1:
+            for coordinate, distance in travels.items():
+                y_distance = abs(coordinate[1] - y_target)
+                if y_distance < distance:
+                    y_rest = distance - y_distance
+                    for i in range(y_rest+1):
+                        point_l = (coordinate[0] - i, y_target)
+                        point_r = (coordinate[0] + i, y_target)
+                        if point_l not in sensors and point_l not in beacons:
+                            y_points.add(point_l)
+                        if point_r not in sensors and point_r not in beacons:
+                            y_points.add(point_r)
+
+            points = []
+            for point in y_points:
+                if point[1] == y_target:
+                    points.append(point[0])
+            print(len(points))
+        else:
+            out_of_bounds = {}
+            for point, distance in travels.items():
+                points = [(point[0] + distance, point[1]), (point[0] - distance, point[1]),
+                          (point[0], point[1] + distance), (point[0], point[1] - distance)]
+                for out_of_bound_point in points:
+                    if out_of_bound_point[0] < 0 or out_of_bound_point[0] > 20:
+                        pass
+                    elif out_of_bound_point[1] < 0 or out_of_bound_point[1] > 20:
+                        pass
+                    else:
+                        if out_of_bounds.get(out_of_bound_point):
+                            out_of_bounds[out_of_bound_point] += 1
+                        else:
+                            out_of_bounds[out_of_bound_point] = 1
+        print(out_of_bounds)
 
 
-main()
+main(part1=False)
