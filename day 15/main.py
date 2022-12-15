df6337c83c54a7a3e5b0452184fd816f7baa2246
Timon Bakker <timonbakker@hotmail.com>
2022-12-15 11:46:29 +0100

    This works, but too much recursion depth

--- day 15/main.py
+++ day 15/main.py
@@ -1,3 +1,8 @@
+sensors = set()
+beacons = set()
+non_beacon = set()
+
+
 def split_line(line):
     sensor, beacon = line.split(":")
     sensor_x, sensor_y = sensor.split(",")
@@ -12,16 +17,35 @@ def split_line(line):
     return (sensor_x, sensor_y), (beacon_x, beacon_y)
 
 
+def manhattan_distance(sensor, beacon):
+    distance = abs(sensor[0] - beacon[0]) + abs(sensor[1] - beacon[1])
+    return distance
+
+
+def travel_sensor(coordinate, distance):
+    points = [(coordinate[0], coordinate[1] + 1), (coordinate[0] + 1, coordinate[1]),
+              (coordinate[0], coordinate[1] - 1), (coordinate[0] - 1, coordinate[1])]
+
+    for point in points:
+        non_beacon.add(point)
+        travel_sensor(point, distance - 1)
+
+
 def main():
-    sensors = []
-    beacons = []
+    global sensors
+    global beacons
+    travels = {}
 
     with open("test.txt", mode="r", encoding="utf8") as file:
         lines = file.read().splitlines()
         for line in lines:
             sensor, beacon = split_line(line)
-            sensors.append(sensor)
-            beacons.append(beacon)
+            sensors.add(sensor)
+            beacons.add(beacon)
+            travels[sensor] = manhattan_distance(sensor, beacon)
+
+        for coordinate, distance in travels.items():
+            travel_sensor(coordinate, distance)
 
 
 main()
